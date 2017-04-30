fce47cb07df0bc706b14c1722e56c713db4eec21
Alan Zimmerman <alan.zimm@gmail.com>
2017-04-30 17:59:41 +0200

    Add more refactorings

--- lsp-haskell.el
+++ lsp-haskell.el
@@ -30,6 +30,16 @@
            `(:start_pos (:position ,(lsp-point-to-position (point))))
            `(:name (:text ,newname)))))
 
+(defun lsp-if-to-case ()
+  "Convert an if statement to a case statement"
+  (interactive)
+  (lsp--cur-workspace-check)
+  (lsp--send-execute-command
+   "hare:iftocase"
+   (vector `(:file      (:textDocument ,(lsp-text-document-identifier)))
+           `(:start_pos (:position     ,(lsp-get-start-position)))
+           `(:end_pos   (:position     ,(lsp-get-end-position))))))
+
 (defun lsp-lift-to-top ()
   "Lift a function to the top level"
   (interactive)
@@ -47,6 +57,44 @@
    "hare:liftonelevel"
    (vector `(:file (:textDocument ,(lsp-text-document-identifier)))
            `(:start_pos (:position ,(lsp-point-to-position (point)))))))
+
+(defun lsp-delete-definition ()
+  "Delete a definition"
+  (interactive)
+  (lsp--cur-workspace-check)
+  (lsp--send-execute-command
+   "hare:deletedef"
+   (vector `(:file (:textDocument ,(lsp-text-document-identifier)))
+           `(:start_pos (:position ,(lsp-point-to-position (point)))))))
+
+(defun lsp-generalise-applicative ()
+  "Generalise a monadic function to use applicative"
+  (interactive)
+  (lsp--cur-workspace-check)
+  (lsp--send-execute-command
+   "hare:genapplicative"
+   (vector `(:file (:textDocument ,(lsp-text-document-identifier)))
+           `(:start_pos (:position ,(lsp-point-to-position (point)))))))
+
+;; ---------------------------------------------------------------------
+
+(defun lsp-haskell--session-cabal-dir ()
+  "Get the session cabal-dir."
+  (let* ((cabal-file (haskell-cabal-find-file))
+         (cabal-dir (if cabal-file
+                        (file-name-directory cabal-file)
+                      "." ;; no cabal file, use directory only
+                      )))
+    (progn
+      (message "cabal-dir: %s" cabal-dir)
+      cabal-dir)))
+
+(defun lsp-haskell--get-root ()
+  "TODO: use projectile directory"
+  (let ((dir (lsp-haskell--session-cabal-dir)))
+    (if (string= dir "/")
+        (user-error (concat "Couldn't find cabal file, using:" dir))
+      dir)))
 
 ;; ---------------------------------------------------------------------
 
